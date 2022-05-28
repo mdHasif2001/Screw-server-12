ad3c6350d6dbe9f44130c61537d746dedf485afa
mdHasif2001 <iamhasib727@gmail.com>
2022-05-28 20:18:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ztdah.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend')
+});
+
+app.listen(port, () => {
+    console.log('My backend server is running on port', port);
+})
+
