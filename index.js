413175ba4da9e51f031c6f28bd49064bb90b9d7b
mdHasif2001 <iamhasib727@gmail.com>
2022-05-28 21:49:42 +0600

    backend server created

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+
 const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ztdah.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
