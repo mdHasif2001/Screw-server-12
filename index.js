f525f69aa897f4bfb25eb99a8644331f2584876b
mdHasif2001 <iamhasib727@gmail.com>
2022-05-30 16:17:14 +0600

    jwt token hasbeen changed

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//Jwt
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
